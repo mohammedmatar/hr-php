5b8b9f9248140b0d5a6ec484ab88d2011322c86d
Carlos Rojas <crojasaragonez@hotmail.com>
2014-12-01 13:56:36 -0600

    enable cors

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'sinatra', '1.4.5'
+gem 'sinatra-cross_origin', '~> 0.3.1'
 gem 'json', '1.7.4'
 gem 'data_mapper', '1.2.0'
 gem 'dm-mysql-adapter', '1.2.0'
--- main.rb
+++ main.rb
@@ -3,16 +3,19 @@ require 'json'
 require 'sinatra'
 require 'data_mapper'
 require 'dm-migrations'
+require 'sinatra/cross_origin'
 
 configure :development do
+  enable :cross_origin
   DataMapper::Logger.new($stdout, :debug)
   DataMapper.setup(
     :default,
-    'mysql://root:12345@localhost/sinatra_service'
+    'postgres://postgres:12345@localhost/sinatra_service'
   )
 end
 
 configure :production do
+  enable :cross_origin
   DataMapper.setup(
     :default,
     'postgres://postgres:12345@localhost/sinatra_service'
